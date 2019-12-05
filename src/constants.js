f51eb4daad1e584ccd206e536fedf97ca3312ea2
Charlie <charlie@Charlies-MBP.home>
2019-12-05 11:27:58 -0500

    remove api key

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,2 @@
-// const API_KEY = <<api_key>>;
-const API_KEY = '60249961fe048e2303317aa3a5696c8f';
+const API_KEY = <<api_key>>;
 export const POP_MOVIE_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
