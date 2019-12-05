3bf7954b18ed2db5b5ed477722aad560f3041c48
Charlie <charlie@Charlies-MBP.home>
2019-12-05 11:46:15 -0500

    add fragment in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from './Header';
 import Main from './Main';
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <Header/>
       <Main/>
-    </div>
+    </Fragment>
   );
 }
 
